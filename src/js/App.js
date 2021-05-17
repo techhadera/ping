f4b8b28fdf6be3fff6bea83bd109c40fc4fbae64
techhadera <rdahaderv@gmail.com>
2021-05-17 17:21:09 +0300

    feat: config params moved to .env

--- src/js/App.js
+++ src/js/App.js
@@ -15,15 +15,12 @@ export default function App() {
       <div className="content-wrapper">
         <Switch>
           <Route path="/settings">
-            {/* <h1>settings view</h1> */}
             <SettingsView />
           </Route>
           <Route path="/login">
-            {/* <h1>login view</h1> */}
             <LoginView />
           </Route>
           <Route path="/register">
-            {/* <h1>register view</h1> */}
             <RegisterView />
           </Route>
           <Route path="/chat/:id">
